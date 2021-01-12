58301365c233fb3b8a8b9b0d447b937bf4971e20
dhiyo7 <dhiyoreksa12@gmail.com>
2021-01-12 22:48:47 +0700

    Add index Route to show Documentation

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,13 @@ const mainRouter = express.Router();
 
 const authRouter = require('./authRouter');
 const tourismRouter = require('./tourismRouter');
+const welcomeRouter = require("./welcome");
 
 const {checkLogin} = require('../helpers/checkUser')
 
 mainRouter.use('/auth', authRouter);
 mainRouter.use('/tourism', checkLogin, tourismRouter);
+mainRouter.use("/", welcomeRouter);
+
 
 module.exports = mainRouter;
